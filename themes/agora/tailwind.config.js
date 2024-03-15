8dd8e732c2bdbd556e40899286bdca3f62551bad
Tsvetan Topalov <tsvetan.topalov@protiviti.bg>
2024-03-15 11:33:58 +0200

    init

---
+++ themes/agora/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'content/**/*.md', 
+    'layouts/**/*.html',
+    '../../layouts/**/*.html',
+    '../../content/**/*.md',
+    './themes/agora/layouts/**/*.html',
+    './themes/agora/content/**/*.md',
+  ],
+  purge: {
+    content: [
+      'content/**/*.md', 
+      'layouts/**/*.html',
+      '../../layouts/**/*.html',
+      '../../content/**/*.md',
+      './themes/agora/layouts/**/*.html',
+      './themes/agora/content/**/*.md',
+    ]
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'agora-green-dark':     '#016270',
+        'agora-green-light':    '#008D91',
+        'agora-talent-light':   '#FFF6D5',
+        'agora-talent-dark':    '#C99603',
+        // 'agora-green-talent':   '#CCE8E9',
+        'agora-company-light':   '#CCE8E9',
+        // 'agora-yellow-company': '#FFF6D5',
+        // Add more custom colors here
+      },
+    },
+  },
+  plugins: [],
+}
